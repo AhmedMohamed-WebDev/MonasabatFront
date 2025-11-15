3d72328e3d9156626ebd2dfd47c1d4fd407283cc
USER <USER@DESKTOP-N3KNHS7>
2025-11-16 00:31:46 +0200

    add new backend url

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -2,8 +2,9 @@
 export const environment = {
   production: true,
   apiUrl:
-    'https://37484aa9-9060-4a7d-985b-fdc8099f1d8c-00-3joc8gi3d72c5.spock.replit.dev/api',
-  // apiUrl: 'https://event-booking-backend-production-0d69.up.railway.app/api',
+    // 'https://37484aa9-9060-4a7d-985b-fdc8099f1d8c-00-3joc8gi3d72c5.spock.replit.dev/api',
+    // apiUrl: 'https://event-booking-backend-production-0d69.up.railway.app/api',
+    'https://event-booking-backend-rard.onrender.com/api',
   appName: 'Event Booking Platform',
   version: '1.0.0',
 
